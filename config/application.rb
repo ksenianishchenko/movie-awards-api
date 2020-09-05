5b05ef60b9a807e2fd44540363f1a9b1e81ddc47
Kseniia Anishchenko <kseninavoi@gmail.com>
2020-09-05 16:57:07 -0700

    added cors

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module MovieAwardsApi
 
     config.api_only = true
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource(
