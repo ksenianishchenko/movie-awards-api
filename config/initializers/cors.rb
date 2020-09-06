f5a235b6a39fd1f5c4c53fe578cd17deef0de494
Kseniia Anishchenko <kseninavoi@gmail.com>
2020-09-06 10:45:25 -0700

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-     origins '*'
-     resource '*', :headers => :any, :methods => [:get, :post, :options]
+     origins 'https://calm-reaches-93104.herokuapp.com'
+     resource '*', :headers => :any, :methods => [:get, :post, :options, :delete, :patch],
+     credentials: true
    end
 end
