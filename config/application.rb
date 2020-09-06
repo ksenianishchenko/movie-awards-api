c8d3bdfae4a23b6c69fc9d2f4af792d11ece6689
Kseniia Anishchenko <kseninavoi@gmail.com>
2020-09-05 17:26:11 -0700

    fixed cors

--- config/application.rb
+++ config/application.rb
@@ -13,15 +13,5 @@ module MovieAwardsApi
 
     config.api_only = true
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource(
-          '*',
-          headers: :any,
-          methods: [:get, :patch, :put, :delete, :post, :options]
-          )
-      end
-    end
   end
 end
