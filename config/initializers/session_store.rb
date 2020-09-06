edff69a68d94c42243b97e80944509ad59106edc
Kseniia Anishchenko <kseninavoi@gmail.com>
2020-09-05 17:22:57 -0700

    fixed cors file

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_movie-awards', domain: 'http://localhost:3000/'
+  Rails.application.config.session_store :cookie_store, key: '_movie-awards', domain: 'https://calm-reaches-93104.herokuapp.com/'
 else
   Rails.application.config.session_store :cookie_store, key: '_movie-awards'
 end
